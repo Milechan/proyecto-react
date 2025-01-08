d55d676f299e92461fa5779b2b9bf9c1acf902ea
milena <milena.concha.m@gmail.com>
2025-01-08 17:43:27 -0300

    agrego footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+function Footer (props){
+    return (
+        <footer className="mi-footer">
+        <a href="mailto:vtuber.miiilechan@gmail.com">vtuber.miiilechan@gmail.com</a>
+        
+        </footer>
+    )
+}
+export default Footer
