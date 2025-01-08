a9347f8faee0b27f32a8841216820ecbd71bf85f
milena <milena.concha.m@gmail.com>
2025-01-08 17:43:08 -0300

    agrego boton con url dinamica

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,14 +5,14 @@ function Card(props) {
     //     cardDescription: 'Descripcion de la primera card',
     //     btnText: 'Haz click aqui'
     //  } 
-    let { cardTitle, cardDescription, btnText, srcImage } = props
+    let { cardTitle, cardDescription, btnText, srcImage, btnUrl} = props
     return (
         <div className="card">
             <img src={srcImage} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{cardTitle}</h5>
                 <p className="card-text">{cardDescription}</p>
-                <a href="#" className="btn btn-primary">{btnText}</a>
+                <a href={btnUrl} target='_blank'  className="btn btn-card-mile">{btnText}</a>
             </div>
         </div>
     )
