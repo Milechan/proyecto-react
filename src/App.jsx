b35538081969bf2281e6eb108d770334b159b4e9
milena <milena.concha.m@gmail.com>
2025-01-08 17:44:27 -0300

    edito informacion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import milemoto from "./assets/milemoto.png"
 import mileuwu from "./assets/mileuwu.png"
 import Navbar from './components/Navbar'
 import Jumbotron from './components/Jumbotron'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,33 +19,52 @@ function App() {
     <div className='principal-container'>
 
       <Navbar />
-      <Jumbotron/>
+      <Jumbotron />
       <div className='card-container mt-5' id='card-container'>
-        <Card
-          cardTitle="Este es el primer titulo"
-          cardDescription="Lorem ipsum dolor sit amet."
-          btnText='Haz click aqui'
-          srcImage={milehappy}
-        />
-        <Card
-          cardTitle="Este es el segundo titulo"
-          cardDescription="Lorem ipsum dolor sit amet."
-          btnText='Haz click aqui'
-          srcImage={milekawai}
-        />
-        <Card
-          cardTitle="Este es el tercer titulo"
-          cardDescription="Lorem ipsum dolor sit amet."
-          btnText='Haz click aqui'
-          srcImage={milemoto}
-        />
-        <Card
-          cardTitle="Este es el cuarto titulo"
-          cardDescription="Lorem ipsum dolor sit amet."
-          btnText='Haz click aqui'
-          srcImage={mileuwu}
-        />
+        <div className="row">
+          <div className="col-lg-3 col-md-6 col-sm-12">
+            <Card
+              cardTitle="Tocando Pasto"
+              cardDescription="Como olvidar ese día en el parque."
+              btnText='Ve A mi IG'
+              srcImage={milehappy}
+              btnUrl="https://www.instagram.com/miiilechan/?hl=es-la"
+            />
+          </div>
+          <div className="col-lg-3 col-md-6 col-sm-12">
+            <Card
+              cardTitle="Un dia en casa"
+              cardDescription="Primera selfi del año!, que felicidad."
+              btnText='Ve a mi TikTok'
+              srcImage={milekawai}
+              btnUrl="https://www.tiktok.com/@miiilechan"
+            />
+          </div>
+          <div className="col-lg-3 col-md-6 col-sm-12">
+            <Card
+              cardTitle="Pi Pi Pii!"
+              cardDescription="Como olvidar cuando me subi a una moto"
+              btnText='Ve a mi X'
+              srcImage={milemoto}
+              btnUrl="https://x.com/miiilechan"
+            />
+          </div>
+          <div className="col-lg-3 col-md-6 col-sm-12">
+            <Card
+              cardTitle="San Valentin"
+              cardDescription="Ese dia estaba nerviosa hablando"
+              btnText='Ve a mi Youtube'
+              srcImage={mileuwu}
+              btnUrl="https://www.youtube.com/@miiilechan"
+            />
+          </div>
+        </div>
+
+
+
+
       </div>
+      <Footer />
     </div>
   )
 }
