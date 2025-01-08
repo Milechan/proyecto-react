57c8d4a82c999c9e52d3cc9ebbd2565c7a54bf8f
milena <milena.concha.m@gmail.com>
2025-01-08 17:43:51 -0300

    modifico contenido jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,9 +3,9 @@ import './Jumbotron.css'
 function Jumbotron(props) {
     return (
         <div className="jumbotron-container">
-            <h1> Bienvenido a mi Pagina!</h1>
-            <p>Mas abajo dejare los links para que sepas mas de mi</p>
-            <a href='#card-container' className="btn btn-primary" > Quieres saber mas de MiiileChan?</a>
+            <h1 className='primeraH'> Bienvenido a mi Pagina Web!</h1>
+            <p className='primeraP'>Mas abajo dejare los links para que sepas mas de mi</p>
+            <a href='https://miiilechan.carrd.co/' target="_blank"  className="btn btn-mile" > Quieres saber mas de MiiileChan?</a>
         </div>
     )
 }
